898ffa8f35a3ae9c6680e34db94e0f6ddc332aee
Mohit-S23 <sharma.mohit0323@gmail.com>
2021-02-02 00:01:18 +0530

    Validating Number Ranges and Regular Expressions

--- spring-mvc-demo/src/com/mohit/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/mohit/springdemo/mvc/Customer.java
@@ -1,6 +1,9 @@
 package com.mohit.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -11,17 +14,45 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@NotNull(message="is required")
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{6}", message="only 6 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+	
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+	
 	
 }
